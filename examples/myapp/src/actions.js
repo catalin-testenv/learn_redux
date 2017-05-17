765a79b8f06bb925c2240f465228ffa7d13dc7a8
Enache Catalin <enachecatalin@Enaches-MacBook-Pro.local>
2017-05-18 00:20:12 +0300

    interval in action - no middleware

--- examples/myapp/src/actions.js
+++ examples/myapp/src/actions.js
@@ -9,6 +9,7 @@ export const setUnit = (unit) => {
     };
 };
 
+// use with middleware
 export const toggleCounter = () => {
     return function(dispatch, getState) {
         dispatch({
@@ -16,6 +17,18 @@ export const toggleCounter = () => {
         });
     };
 };
+
+// with no middleware
+// export const toggleCounter = () => {
+//     return function(dispatch, getState) {
+//         const { started } = getState().counter;
+//         started && clearInterval(started);
+//         dispatch({
+//             type: 'TOGGLE_COUNTER',
+//             started: started ? null : setInterval(() => dispatch(tick()), 1000)
+//         });
+//     };
+// };
 
 export const tick = () => {
     return function(dispatch, getState) {
